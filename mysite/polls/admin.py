8a7139ae1e7a7b865d7b30d2b1f09ea2ad263abd
hyun3607 <iou3607@gmail.com>
2022-07-03 16:41:08 +0900

    part7 TabularInline

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # Register your models here.
-class ChoiceInline(admin.StackedInline):
-    model = choice
+class ChoiceInline(admin.TabularInline):
+    model = Choice
     extra = 3
     
 class QuestionAdmin(admin.ModelAdmin):
