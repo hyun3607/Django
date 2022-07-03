cdc534b41958c086dad7a34cbefc6709c93861ee
hyun3607 <iou3607@gmail.com>
2022-07-03 16:18:56 +0900

    part7 fieldsets

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,10 @@ from .models import Question, Choice
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fields = [
+        (None,                {'fields': ['question_text']}),
+         ('Date information', {'fields': ['pub_date']}),
+    ]
     
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
